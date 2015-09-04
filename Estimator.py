f6e802c2d1d574cba6a1660e380b233450e61345
Neeraj Kashyap <nkashy1@gmail.com>
2015-09-04 16:40:28 -0700

    Finished testing of Estimator. Now to try the script out!

---
+++ Estimator.py
@@ -0,0 +1,70 @@
+from __future__ import print_function
+import numpy as np
+
+
+class Estimator(object):
+    def __init__(self, distribution):
+        scale = len(distribution)
+        self.probability_values = np.arange(scale, dtype=float)/(scale-1)
+        self.distribution = distribution
+        self.ones = np.ones(self.distribution.shape)
+
+    def update(self, record):
+        if record == 1:
+            dot = self.probability_values*self.distribution
+            self.distribution = dot/dot.sum()
+        else:
+            dot = (self.ones - self.probability_values)*self.distribution
+            self.distribution = dot/dot.sum()
+
+    def save(self, statsfile):
+        with open(statsfile, 'w') as outfile:
+            for p in list(self.distribution):
+                print(str(p), file=outfile)
+
+    def sample(self):
+        cumsum = self.distribution.cumsum()
+        uniform_sample = np.random.uniform()
+        return np.argmin(cumsum >= uniform_sample)/(cumsum.size+1)
+
+    def expectation(self):
+        return (self.probability_values*self.distribution).sum()
+
+
+# TESTS
+import unittest
+
+class EstimatorTest0(unittest.TestCase):
+    def setUp(self):
+        distribution = np.array([0.25, 0.25, 0.25, 0.25])
+        self.estimator = Estimator(distribution)
+        self.scale = len(distribution)
+
+    def test_constructor(self):
+        estimator = self.estimator
+
+        expected_probability_values = np.array([0, 1, 2, 3], dtype=float)/3
+        for j in range(self.scale):
+            self.assertEqual(estimator.probability_values[j], expected_probability_values[j])
+
+        expected_distribution = np.array([0.25, 0.25, 0.25, 0.25])
+        for j in range(self.scale):
+            self.assertEqual(estimator.distribution[j], expected_distribution[j])
+
+        self.assertEqual(estimator.ones.size, 4)
+
+    def test_update_win(self):
+        estimator = self.estimator
+        estimator.update(1)
+        self.assertAlmostEqual(estimator.distribution[0], 0)
+        self.assertAlmostEqual(estimator.distribution[1], 1.0/6)
+        self.assertAlmostEqual(estimator.distribution[2], 1.0/3)
+        self.assertAlmostEqual(estimator.distribution[3], 1.0/2)
+
+    def test_update_loss(self):
+        estimator = self.estimator
+        estimator.update(0)
+        self.assertAlmostEqual(estimator.distribution[3], 0)
+        self.assertAlmostEqual(estimator.distribution[2], 1.0/6)
+        self.assertAlmostEqual(estimator.distribution[1], 1.0/3)
+        self.assertAlmostEqual(estimator.distribution[0], 1.0/2)
--- handicap.py
+++ handicap.py
@@ -1,2 +1,42 @@
+from __future__ import print_function
 import numpy as np
 
+
+# Script set-up
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description="Estimate the Handicapper's skill at his game.")
+parser.add_argument('--statsfile')
+parser.add_argument('records', type=argparse.FileType('r'))
+parser.add_argument('--outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+parser.add_argument('-s', type=int)
+
+args = parser.parse_args()
+
+if args.statsfile is None:
+    significant_digits = 3
+    if args.s is not None:
+        significant_digits = args.s
+    num_candidates = 1 + 10**significant_digits
+    distribution = (1/float(num_candidates))*np.ones(num_candidates, dtype=float)
+else:
+    with open(args.statsfile, 'r') as statsfile:
+        lines = list(statsfile)
+    significant_digits = int(lines[0])
+    distribution = np.array(lines[1:], dtype=float)
+
+
+# Heavy lifting
+from Estimator import Estimator
+
+estimator = Estimator(distribution)
+records = list(args.records)
+
+for record in records:
+    estimator.update(record)
+
+if args.statsfile is not None:
+    estimator.save(statsfile)
+
+print(str(estimator.expectation()), f=args.outfile)
